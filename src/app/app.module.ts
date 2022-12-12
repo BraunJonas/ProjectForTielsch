371b39ae93b43e25166e1705ff6dbe3d42406456
BuildTools <unconfigured@null.spigotmc.org>
2022-12-12 16:50:09 +0100

    firstCommitJonas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,53 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+
+
+
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomescreenComponent } from './homescreen/homescreen.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProgrammComponent } from './programm/programm.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { SpecialsComponent } from './specials/specials.component';
+import { AnfahrtComponent } from './anfahrt/anfahrt.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomescreenComponent,
+    ProgrammComponent,
+    SpecialsComponent,
+    AnfahrtComponent,
+    ImpressumComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    FormsModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
